0a4f3ec23e48b80ea5e3667cbed3c78b8ba906b0
haoshuhu <haoshuhu@bingo-yes.com.cn>
2021-03-04 17:29:58 +0800

    类继承时父类和子类的构造方法 is ok

--- oop.py
+++ oop.py
@@ -1,1 +1,26 @@
-#instanceof
+import sys
+
+class Animal:
+    def __init__(self,name):
+        self.name = name
+        print("created an animal object:"+name);
+    def eat(self,food):
+        print("an animal: "+self.name+ " can eat :"+food);
+
+class Cat(Animal):
+    def __init__(self,name):
+        super(Cat, self).__init__(name)
+        print("create a cat object")
+    def catchMouce(self,size):
+        print("the cat{} catch a {} mouce".format(self.name,size))
+
+if __name__ == "__main__":
+    print ('This is main of module "oop.py"')
+    C =Cat("tom")
+    C.catchMouce("big")
+    print(hasattr(C,"catchMouce"))
+    print(hasattr(C, "eat"))
+    print(hasattr(C, "sleep"))
+    fn=getattr(C,"eat")
+    fn("jerry")
+
