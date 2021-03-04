8f31f08b9ea76452cb141dc35a3ddc6d53004c54
haoshuhu <haoshuhu@bingo-yes.com.cn>
2021-03-04 15:13:47 +0800

    mongodb ok

---
+++ databbase_mongo.py
@@ -0,0 +1,12 @@
+import pymongo
+
+myclient = pymongo.MongoClient('mongodb://{}:{}@{}:{}/?authSource={}'.format("root","root","localhost","27017","efs"))
+
+mydb = myclient.efs
+k = mydb.collection_names(include_system_collections=True)  # 返回当前库下所有的collection名
+print(k)
+
+
+mycol = mydb["report"]
+for x in mycol.find():
+  print(x)
